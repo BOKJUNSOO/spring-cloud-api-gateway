3821a7b458fdfd89f587ecd56ffabb40aec5d097
복준수 <brianbok97@gmail.com>
2025-05-30 14:44:07 +0900

    2025-05-30 (1)

---
+++ src/main/java/com/welab/api_gateway/secret/jwt/props/JwtConfigProperties.java
@@ -0,0 +1,14 @@
+package com.welab.api_gateway.secret.jwt.props;
+
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.stereotype.Component;
+
+@Component
+@ConfigurationProperties(value = "jwt", ignoreUnknownFields = true)@Getter
+@Setter
+public class JwtConfigProperties {
+    private String header;
+    private String secretKey;
+}
